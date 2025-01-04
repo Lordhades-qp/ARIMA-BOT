bcb953aac3fb6c51de58f51695904746ba8ca8f4
LORD HADÈS ©T☢️ <hadeslord087@gmail.com>
2025-01-04 17:31:40 +0000

    Create Json.js

---
+++ commands/groupmenu/Json.js
@@ -0,0 +1,38 @@
+{
+  "groupSettings": {
+    "default": {
+      "welcomeMessage": "Bienvenue dans le groupe!",
+      "allowLinks": true,
+      "silenceMode": false,
+      "adminOnlyCommands": false
+    }
+  },
+  "groups": {
+    "1203630254@g.us": {
+      "name": "Groupe Test",
+      "admins": [
+        "user1@s.whatsapp.net",
+        "user2@s.whatsapp.net"
+      ],
+      "settings": {
+        "welcomeMessage": "Bienvenue dans notre groupe ! Merci de respecter les règles.",
+        "allowLinks": false,
+        "silenceMode": true,
+        "adminOnlyCommands": true
+      }
+    },
+    "1203630255@g.us": {
+      "name": "Groupe Amis",
+      "admins": [
+        "user3@s.whatsapp.net",
+        "user4@s.whatsapp.net"
+      ],
+      "settings": {
+        "welcomeMessage": "Salut tout le monde, amusez-vous bien ici !",
+        "allowLinks": true,
+        "silenceMode": false,
+        "adminOnlyCommands": false
+      }
+    }
+  }
+}
