69e5d368c618b66dfb4f193b124b5612e99b8e7e
LORD HADÈS ©T☢️ <hadeslord087@gmail.com>
2025-01-04 13:17:23 +0000

    Delete Framework/scr/routes directory

--- Framework/scr/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const botController = require('../controllers/botController');
-
-router.get('/status', botController.getStatus);
-router.post('/message', botController.sendMessage);
-
-module.exports = router;
